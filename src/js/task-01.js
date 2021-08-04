a5eee6738ece9eac30466f4ecc03e35751bf1121
Alina Kornienko <a_kornienko@ukr.net>
2021-08-04 22:43:19 +0300

    task 1-2

---
+++ src/js/task-01.js
@@ -0,0 +1,5 @@
+const categEl = document.querySelector('ul');
+console.log('categEl', categEl);
+// console.log(categEl.children[1]);
+console.log(categEl.textContent);
+
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const listEl = document.createElement('ul');
+listEl.classList.add('ingredients');
+console.log(listEl);
+document.body.appendChild(listEl);
+
+const.element = ingredients.map (option => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  console.log(itemEl.textContent);
+  
+
+  return itemEl;
+});
+// const itemEl = document.createElement('li');
+// itemEl.classList.add('item');
+// console.log(itemEl.textContent);
+// itemEl.textContent = ingredients[0];
+// console.log(itemEl);
+// document.body.appendChild(itemEl);
+
+// listEl.appendChild(itemEl); 
+// console.log(listEl);
