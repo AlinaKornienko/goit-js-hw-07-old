fd1d8ffa78dea322191b17244c5653ef30df7b31
Alina Kornienko <a_kornienko@ukr.net>
2021-08-11 17:31:21 +0300

    Update task #4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,9 +3,11 @@ const counterValue = document.querySelector('#value');
 const buttonDecrement = counter.firstElementChild;
 const buttonIncrement = counter.lastElementChild;
 
-buttonDecrement.addEventListener('click', () => {
+function onButtonDecrementClick (){
     counterValue.textContent -=1;
-});
-buttonIncrement.addEventListener('click', () => {
+};
+function onButtonIncrementClick (){
     counterValue.textContent = Number(counterValue.textContent)+1;
-});
+};
+buttonDecrement.addEventListener('click', onButtonDecrementClick);
+buttonIncrement.addEventListener('click', onButtonIncrementClick);
