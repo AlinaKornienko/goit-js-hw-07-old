15078c25c7800ee8c53d1ec7e2961018e3a38900
Alina Kornienko <a_kornienko@ukr.net>
2021-08-11 12:36:37 +0300

    js-task#2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,25 +6,16 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
-const listEl = document.createElement('ul');
-listEl.classList.add('ingredients');
-console.log(listEl);
-document.body.appendChild(listEl);
 
-const.element = ingredients.map (option => {
-  const itemEl = document.createElement('li');
-  itemEl.classList.add('item');
-  console.log(itemEl.textContent);
+const ingredientsEl = document.querySelector('#ingredients');
+
+const createIngredientsList = options => {
+  return options.map(option => {
+    const ingredientItem = document.createElement('li');
+    ingredientItem.textContent = option;
   
-
-  return itemEl;
-});
-// const itemEl = document.createElement('li');
-// itemEl.classList.add('item');
-// console.log(itemEl.textContent);
-// itemEl.textContent = ingredients[0];
-// console.log(itemEl);
-// document.body.appendChild(itemEl);
-
-// listEl.appendChild(itemEl); 
-// console.log(listEl);
+    return ingredientItem;
+    });
+};
+const elements = createIngredientsList(ingredients);
+ingredientsEl.append(...elements);
