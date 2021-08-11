e576b33e58f2f43edb25f630b050a6cb2006ae00
Alina Kornienko <a_kornienko@ukr.net>
2021-08-11 20:25:01 +0300

    Update task#5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -4,5 +4,7 @@ const refs = {
 };
 refs.input.addEventListener('input', onInputChange);
 function onInputChange(event) {
-    console.log(currentTarget.value);
+    console.log(event.currentTarget.value);
+    refs.nameLabel.textContent = event.currentTarget.value;
+    spanEl.textContent = input.value ? input.value : 'незнакомец';
 };
