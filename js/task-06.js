71ee4ea9c0ea379dd6a5cd2a275673dc609ac2b3
Alina Kornienko <a_kornienko@ukr.net>
2021-08-16 13:07:20 +0300

    Updated task-6

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const validationInput = document.querySelector('#validation-input');
 
 const onInputBlur = () => {
     validationInput.classList.forEach(elem => validationInput.classList.remove(elem));
-  if (validationInput.value.length === parseInt(validationInput.dataset.length, 10)) {
+  if (validationInput.value.length === parseInt(validationInput.dataset.length)) {
     validationInput.classList.add('valid');
   } else {
     validationInput.classList.add('invalid');
